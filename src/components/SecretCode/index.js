f947021ba83f49dc5fa1a2604dc33427595857e9
Natacha <natacha.dehass@gmail.com>
2020-05-27 15:44:41 +0200

    clean CSS

--- src/components/SecretCode/index.js
+++ src/components/SecretCode/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './index.css';
+import style from './style.module.css';
 import { NextButton } from '../NextButton';
 import { useHistory } from 'react-router-dom';
 
@@ -32,44 +32,47 @@ export function SecretCode() {
                 <div>
                     <br />
                     <NextButton
-                        text=">> Next"
+                        text=">> Prochaine Enigme"
                         handleClick={handleRedirection}
                     />
                 </div>
             );
         } else if (proposition === 'false') {
-            return <p className="wrongAnswer"> ...Essaye encore... </p>;
+            return <p className={style.wrongAnswer}> ...Essaye encore... </p>;
         } else {
         }
     }
     return (
-        <div className="globalSecretCode">
+        <div className={style.globalSecretCode}>
             <h1>LE CODE SECRET </h1>
-            <div className="gameBlock">
-                <p className="explanations">
+            <div className={style.gameBlock}>
+                <p className={style.explanations}>
                     Trouve le code secret pour passer à la prochaine énigme...
                 </p>
 
                 <p>
-                    4U D3P4r<span className="secretLetter">7</span> C357 8451QU3
-                    r1<span className="secretLetter">3</span>N D3
-                    <span className="secretLetter">N</span>0UV34U N1 D3 C0MP11QU
-                    3 M415 r4P1D3M3N
-                    <span className="secretLetter">7</span> C4 V
-                    <span className="secretLetter">4</span> 53 6473r 50 1
-                    <span className="secretLetter">7</span> P47
-                    <span className="secretLetter">1</span>3N7 P
-                    <span className="secretLetter">0</span>Ur 646
-                    <span className="secretLetter">N</span>3r
+                    4U D3P4r<span className={style.secretLetter}>7</span> C357
+                    8451QU3 r1<span className={style.secretLetter}>3</span>N D3
+                    <span className={style.secretLetter}>N</span>0UV34U N1 D3
+                    C0MP11QU 3 M415 r4P1D3M3N
+                    <span className={style.secretLetter}>7</span> C4 V
+                    <span className={style.secretLetter}>4</span> 53 6473r 50 1
+                    <span className={style.secretLetter}>7</span> P47
+                    <span className={style.secretLetter}>1</span>3N7 P
+                    <span className={style.secretLetter}>0</span>Ur 646
+                    <span className={style.secretLetter}>N</span>3r
                 </p>
                 <input
                     onChange={handleTermChange}
-                    className="userInput"
+                    className={style.userInput}
                 ></input>
-                <button onClick={handleClick} className="validationButton">
+                <button
+                    onClick={handleClick}
+                    className={style.validationButton}
+                >
                     Tenter ma chance
                 </button>
-                <div>{getResults()}</div>
+                <div className={style.nextButton}>{getResults()}</div>
             </div>
         </div>
     );
