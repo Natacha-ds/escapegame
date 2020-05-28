17adda48754ca147ea1518ad6da8fb46d66342fb
Natacha <natacha.dehass@gmail.com>
2020-05-28 16:02:20 +0200

    nouvelle version secret exit

--- src/components/ExitBlock/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-import style from './style.module.css';
-
-export function ExitBlock({ text }) {
-    return <div className={style.pageBlock}>{text}</div>;
-}
--- src/components/SecretExit/index.js
+++ src/components/SecretExit/index.js
@@ -1,88 +1,22 @@
 import React, { useState } from 'react';
 import style from './style.module.css';
-import { ExitBlock } from '../ExitBlock/index';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 export function SecretExit() {
-    const history = useHistory();
-    function bottomArrow() {
-        return <div className={style.center}>↓</div>;
-    }
-
-    function diagonalArrow() {
-        return <div className={style.center}>↘︎</div>;
-    }
-
-    function rightArrow() {
-        return <div className={style.center}>→</div>;
-    }
-
-    function cross() {
-        return (
-            <div>
-                <div className={style.center}>
-                    <span onClick={onClick}>⤫ </span>{' '}
-                </div>
-            </div>
-        );
-    }
-
-    function onClick() {
-        history.push('/'); // a modifier quand j'aurais la bonne redirection
-    }
-
     return (
         <div className={style.globalSecretExitPage}>
             <h1 className={style.whiteh1}>L'ECHAPPATOIRE </h1>
             <p className={style.explanations}>
                 Toute sortie est une entrée quelque part ailleurs
             </p>
-
-            <div className={style.block}>
-                <ExitBlock text={bottomArrow()} />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-            </div>
-            <div className={style.block}>
-                <ExitBlock text={diagonalArrow()} />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-            </div>
-            <div className={style.block}>
-                <ExitBlock />
-                <ExitBlock text={rightArrow()} />
-                <ExitBlock text={rightArrow()} />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-            </div>
-            <div className={style.block}>
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock text={diagonalArrow()} />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-            </div>
-            <div className={style.block}>
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock />
-                <ExitBlock text={rightArrow()} />
-                <ExitBlock text={rightArrow()} />
-                <ExitBlock text={cross()} />
-            </div>
+            <div className={style.downArrow}>↓</div>
+            <div className={style.secondDownArrow}>↓</div>
+            <div className={style.diagonaleArrow}>↘︎</div>
+            <div className={style.rightArrow}>→</div>
+            <div className={style.secondDiagonaleArrow}>↘︎</div>;
+            <Link className={style.exitCross} to="/">
+                ⤫
+            </Link>
         </div>
     );
 }
